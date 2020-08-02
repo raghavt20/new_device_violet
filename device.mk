3639919b9c4710dc5ed656e19fcdebf47f4245bb
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2020-08-02 18:47:00 +0530

    Build Some FS Packages
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FastBoot
 
+# FS
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fstman \
+    fstman.ini \
+    setup_fs \
+    make_ext4fs
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
